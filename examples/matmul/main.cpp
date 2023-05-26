e4bca8041881093541977798c98e94543efa7365
Luc Rosenzweig <lucrosenzweig@DN0a2266b5.SUNet>
2023-05-26 01:25:47 -0700

    bla

--- examples/matmul/main.cpp
+++ examples/matmul/main.cpp
@@ -1,13 +1,15 @@
+#include <nezha/time.hpp>
 #include <nezha/graph.hpp>
 #include <nezha/gpu_context.hpp>
 
-#define SHAPE_M       (64*3*10)
-#define SHAPE_N       (32*3*40)
-#define SHAPE_K       (100)
+
+#define SHAPE_M       (640*640)
+#define SHAPE_N       (32*32)
+#define SHAPE_K       (320)
 
 #define BLOCK_ITEMS_M (64)
 #define BLOCK_ITEMS_N (32*2)
-#define BLOCK_ITEMS_K (10)
+#define BLOCK_ITEMS_K (20)
 
 // These are things which are derived from user defined values.
 // We round up the block items constants.
@@ -25,12 +27,14 @@ int main(int argc, char **argv)
     { .size = SHAPE_M * SHAPE_K * sizeof(float), .host_visible = true, .type = nz::binding::type::storage_buffer });
   nz::gpu_buffer_ref mat_b = graph.register_buffer(
     { .size = SHAPE_K * SHAPE_N * sizeof(float), .host_visible = true, .type = nz::binding::type::storage_buffer });
+  nz::gpu_buffer_ref mat_out = graph.register_buffer(
+    { .size = SHAPE_M * SHAPE_N * sizeof(float), .host_visible = true });
 
   { // Initialize mat a
     nz::memory_mapping map_a = graph.get_buffer(mat_a).map();
     float *data = (float *)map_a.data();
     for (int i = 0; i < SHAPE_M * SHAPE_K; ++i)
-      data[i] = (float)i + 10.0f;
+      data[i] = (float)i;
 
     nz::memory_mapping map_b = graph.get_buffer(mat_b).map();
     data = (float *)map_b.data();
@@ -44,12 +48,17 @@ int main(int argc, char **argv)
       .set_kernel(kernel)
       .add_storage_buffer(mat_a)
       .add_storage_buffer(mat_b)
+      .add_storage_buffer(mat_out)
       .dispatch(BLOCK_COUNT_N, BLOCK_COUNT_M, 1);
   }
   nz::job job = graph.end();
 
+  nz::time_stamp start = nz::current_time();
   auto workload = graph.submit(job);
   workload.wait();
+  nz::time_stamp end = nz::current_time();
+
+  nz::log_info("Time %f", nz::time_difference(end, start));
 
   return 0;
 }
